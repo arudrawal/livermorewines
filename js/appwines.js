0d745f88c0fc4216ef92578f9179ee2882545fe2
AjayR <arudrawal@hrforhealth.com>
2017-01-08 14:01:49 -0800

    add google map

---
+++ js/appwines.js
@@ -0,0 +1,20 @@
+var googleMapApiKey = "AIzaSyB7odyxqSylhrPYpmNw6xg65tHCdKghI18";
+
+var wineries = [
+        {name: 'Wente Wineries',
+         maploc: '',
+         imgAttrib: 'images/cat1.jpg',
+          nickNames: ['A1', 'B1', 'C1', 'D1']},
+        {clickCount: 0, name: 'Pussy', 
+         imgSrc: 'images/cat2.jpg', imgAttrib: 'images/cat2.jpg',
+          nickNames: ['A2', 'B2', 'C2', 'D2']},          
+        {clickCount: 0, name: 'Buchhi', 
+         imgSrc: 'images/cat3.jpg', imgAttrib: 'images/cat3.jpg',
+          nickNames: ['A3', 'B3', 'C3', 'D3']},
+        {clickCount: 0, name: 'Luchhi', 
+         imgSrc: 'images/cat4.jpg', imgAttrib: 'images/cat4.jpg',
+          nickNames: ['A4', 'B4', 'C4', 'D4']},
+        {clickCount: 0, name: 'Pungai', 
+         imgSrc: 'images/cat5.jpg', imgAttrib: 'images/cat5.jpg',
+          nickNames: ['A5', 'B5', 'C5', 'D5']},
+];
