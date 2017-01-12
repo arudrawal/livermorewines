51ab61cf68e9785ba9aaca9364e5fa6301f00f10
AjayR <arudrawal@hrforhealth.com>
2017-01-11 22:49:52 -0800

    Add filter function.

--- js/appwines.js
+++ js/appwines.js
@@ -1,68 +1,109 @@
 var googleMapApiKey = "AIzaSyB7odyxqSylhrPYpmNw6xg65tHCdKghI18";
 
-var initalListWineries = [
+var wineriesModel = [
         {name: 'Concannon Vineyard',
          addr: '4590 Tesla road, Livermore',
          phone: '(800)258-9866',
          lat: 37.666217,
-         lng: -121.739736},
+         lng: -121.739736,
+         clickcount: 0},
         
         {name: 'Wente Vineyard',
          addr: '5565 Tesla road, Livermore',
          phone: '(925)456-0439',
          lat: 37.664250,
-         lng: -121.725347},
+         lng: -121.725347,
+         clickcount: 0},
 
         
         {name: 'Las Positas Vineyards',
          addr: '1828 Wetmore road, Livermore',
          phone: '(925)449-9463',
          lat: 37.645922,
-         lng: -121.770524},
+         lng: -121.770524,
+         clickcount: 0},
         
         {name: 'Garre Vineyard & Winery',
          addr: '7986 Tesla road, Livermore',
          phone: '(925)371-8200',
          lat: 37.636250,
-         lng: -121.557990},
+         lng: -121.557990,
+         clickcount: 0},
         
         {name: 'Retzlaff Vineyards',
          addr: '1356 S Livermore Ave, Livermore',
          phone: '(925)447-8941',
          lat: 37.673628,
-         lng: -121.750495},
+         lng: -121.750495,
+         clickcount: 0},
         
         {name: '3 Steves Winery',
          addr: '5700 Greenville Rd, Livermore',
          phone: '(925)997-7736',
          lat: 37.648543,
-         lng: -121.694541},
+         lng: -121.694541,
+         clickcount: 0},
          
         {name: 'Crooked Vine Winery',
          addr: '4948 Tesla Rd, Livermore',
          phone: '(925)449-0458',
          lat: 37.666327,
-         lng: -121.733695},
+         lng: -121.733695,
+         clickcount: 0},
 
 ];
+/*
+var Winery = function(data) {
+    this.name = data.name;
+    this.addr = data.addr;
+    this.phone = data.phone;
+    this.lat = data.lat;
+    this.lng = data.lng;
+    this.clickcount = ko.observable(data.clickcount);
+};*/
 var Winery = function(data) {
     this.name = ko.observable(data.name);
     this.addr = ko.observable(data.addr);
     this.phone = ko.observable(data.phone);    
     this.lat = ko.observable(data.lat);
     this.lng = ko.observable(data.lng);
+    this.clickcount = ko.observable(data.clickcount);
 };
-
+// Consider a filter for the list.
 var viewModel = function (){
     self = this; // use self with surity of context
     this.wineryList = ko.observableArray([]);
-    initalListWineries.forEach(function (wineryItem){
+    wineriesModel.forEach(function (wineryItem){
         self.wineryList.push(new Winery(wineryItem));
     });
-    this.currentWinery = ko.observable(this.wineryList()[0]);
+    this.filterList = function (formElement){
+            var feFilter = document.getElementById('filter');
+            var stringFilter = feFilter.value;
+            var exp = "/" + stringFilter + "/";            
+            itemCount = len = wineriesModel.length;
+            //var filteredList = ko.observableArray([]);
+            self.wineryList.removeAll();
+            for (idx=0; idx < itemCount; idx++){
+            if (stringFilter.length <= 0 || stringFilter==='') {
+                    self.wineryList.push(new Winery(wineriesModel[idx]));
+                } else {
+                    var name = wineriesModel[idx].name;
+                    //if (name.search(exp) >= 0){ // found
+                    if (name.indexOf(stringFilter) !== -1){
+                        self.wineryList.push(new Winery(wineriesModel[idx]));
+                    }
+                }
+            }
+            console.log('filterList called with filter:' + stringFilter);
+    }
+    //this.currentWinery = ko.observable(this.wineryList()[0]);
+    //this.incrementCounter = function() {
+    //    self.currentWinery().clickCount( self.currentWinery().clickCount()+1);
+    //}
+    /*
     this.selectWinery = function(wineryItem) {
         console.log(wineryItem.name());
         self.currentWinery(wineryItem);
-    }
+    }*/
 }
-//ko.applyBindings(new viewModel());
+ko.applyBindings(new viewModel());
